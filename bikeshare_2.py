1db11dc20fbc228517080b3b9cb5812fa204fd93
Diem,Chau <Diemctn@fsoft.com.vn>
2022-12-05 15:01:41 +0700

    Update bikeshare.py (runable version)

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -69,19 +69,77 @@ def get_filters():
     return city, month, day
 
 
+def load_data(city, month, day):
+    """
+    Loads data for the specified city and filters by month and day if applicable.
+
+    Args:
+        (str) city - name of the city to analyze
+        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    Returns:
+        df - Pandas DataFrame containing city data filtered by month and day
+    """
+    # Load csv file as dataframe
+    df = pd.read_csv(city)
+    
+    # Create new columns for filter later
+    df['Start Time'] = pd.to_datetime(df['Start Time'], format='%Y-%m-%d %H:%M:%S')
+
+    df['Month'] = df['Start Time'].dt.month
+    df['Weekday Name'] = df['Start Time'].dt.day_name()
+    df['Start Hour'] = df['Start Time'].dt.hour
+
+
+    # filter by month if applicable
+    if month != 'all':
+        df = df.loc[df['Month']==int(month)]
+
+    # filter by day of week if applicable
+    if day != 'all':
+        df = df.loc[df['Weekday Name']==day.title()]
+
+    # Prompt user if they want to see the data
+    iter = 0
+    step = 5
+    number_of_rows = len(df)
+    maximum_iter = np.ceil(number_of_rows / step)
+    while True:
+        if iter == 0:
+            view_data = input("Do you want to take a look at the data. Type 'y' or 'yes' to see first 5 rows. Type 'n' to quit: ")
+        elif iter > 0:
+            view_data = input("Do you want to continue view next 5 rows?. Type 'y' or 'yes' to see first 5 rows. Type 'n' to quit: ")
+        
+        if view_data in ['y', 'yes']:
+            if iter == maximum_iter:
+                print(df.iloc[iter*step:])
+            else:
+                print(df.iloc[iter*step:(iter+1)*step])
+            iter += 1
+        elif view_data == 'n':
+            break
+        else:
+            print("Please type valid answer")
+    return df
+
+
 def time_stats(df):
     """Displays statistics on the most frequent times of travel."""
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
 
-    # display the most common month
+    # TO DO: display the most common month
+    most_common_month = df['Month'].mode()[0]
+    print("Most common month: ", get_key_from_value(MONTH_MAPPING, most_common_month).title())
 
+    # TO DO: display the most common day of week
+    most_common_weekday = df['Weekday Name'].mode()[0]
+    print("Most common weekday: ", most_common_weekday)
 
-    # display the most common day of week
-
-
-    # display the most common start hour
+    # TO DO: display the most common start hour
+    most_common_hour = df['Start Hour'].mode()[0]
+    print("Most common hour: ", most_common_hour)
 
 
     print("\nThis took %s seconds." % (time.time() - start_time))
@@ -94,14 +152,18 @@ def station_stats(df):
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
 
-    # display most commonly used start station
+    # TO DO: display most commonly used start station
+    most_common_start_station = df['Start Station'].mode()[0]
+    print("Most common start station: ", most_common_start_station)
 
+    # TO DO: display most commonly used end station
+    most_common_end_station = df['End Station'].mode()[0]
+    print("Most common end station: ", most_common_end_station)
 
-    # display most commonly used end station
-
-
-    # display most frequent combination of start station and end station trip
-
+    # TO DO: display most frequent combination of start station and end station trip
+    start_end_station = df['Start Station'] + ' --- ' + df['End Station']
+    most_common_start_end_station = start_end_station.mode()[0]
+    print("Most common start-end station: ", most_common_start_end_station)
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
@@ -113,10 +175,15 @@ def trip_duration_stats(df):
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
 
-    # display total travel time
+    # TO DO: display total travel time
+    total_travel_time = df['Trip Duration'].sum()
+    total_h, total_m = divmod(total_travel_time, 60)
+    print(f'Total Travel Time: {total_travel_time} seconds ~ {int(total_h):,} hour and {int(total_m)} minutes')
 
-
-    # display mean travel time
+    # TO DO: display mean travel time
+    avg_travel_time = df['Trip Duration'].mean()
+    avg_h, avg_m = divmod(avg_travel_time, 60)
+    print(f'Average Travel Time: {avg_travel_time} seconds ~ {int(avg_h):,} hour and {int(avg_m)} minutes')
 
 
     print("\nThis took %s seconds." % (time.time() - start_time))
@@ -129,14 +196,35 @@ def user_stats(df):
     print('\nCalculating User Stats...\n')
     start_time = time.time()
 
-    # Display counts of user types
+    # TO DO: Display counts of user types
+    user_type_count = df['User Type'].value_counts()
+    print('Counts of user types: \n', user_type_count)
+    print('-'*40)
+
+    # TO DO: Display counts of gender
+    try:
+        gender_count = df['Gender'].value_counts()
+        print('Counts of gender: \n', gender_count)
+        print('-'*40)
+    except KeyError:
+        print('This file does not have column Gender, skip checking count of gender!')
 
 
-    # Display counts of gender
+    # TO DO: Display earliest, most recent, and most common year of birth
+    try:
+        df['Birth Year']
+    except KeyError:
+        print("This file does not have column Birth Year, skip finding statistics for birth year")
+    else: 
+        earliest_yob = int(df['Birth Year'].min())
+        print('Earliest year of birth:', earliest_yob)
 
+        most_recent_yob = int(df['Birth Year'].max())
+        print('Most recent year of birth: ', most_recent_yob)
 
-    # Display earliest, most recent, and most common year of birth
-
+        most_common_yob = int(df['Birth Year'].mode()[0])
+        print('Most common year of birth: ', most_common_yob)
+    
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
